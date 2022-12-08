9aba52556debc02b90ff072d631dc57b2bec708f
ZaurKhakurinov <lazter@mail.ru>
2022-12-09 00:54:38 +0300

    Задача 4. проблемы с синтаксисом или VScode

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear ();
+Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+Console.Write("c:");
+int c = int.Parse(Console.ReadLine());
+max = a;
+if (max > b)
+{ 
+    max > c;
+}max == b;
+If (max > c);
+{
+    Console.WriteLine($"амксимальное значение {max}");
+}max == c;
+    
+Console.WriteLine($"большее значение {max}");
